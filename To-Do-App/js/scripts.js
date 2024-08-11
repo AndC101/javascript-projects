3cfebeb7ef259938fd83d1860d0d3a454c09aa5d
Andrew Chen <andrew07.chen@gmail.com>
2024-08-11 11:47:07 -0400

    created new files

--- To-Do-App/js/scripts.js
+++ To-Do-App/js/scripts.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("test").innerHTML = "paragraph changed";
 }
+
+function loadTask() {
+    let element = document.createElement("h1");
+    element.innerHTML="Testing add.";
+    document.getElementById("#testing").append(element);
+}
